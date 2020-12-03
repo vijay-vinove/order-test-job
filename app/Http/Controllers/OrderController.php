3aaffd8eab651e8995cf76a51b1fc29f584d1c76
Vijay Gautam <vijay.gautam@mail.vinove.com>
2020-12-03 19:44:05 +0530

    add validation

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Services\Interfaces\OrderServiceInterface;
 use App\Models\Order;
 use App\Models\Item;
+use Illuminate\Support\Facades\Validator;
 
 class OrderController extends Controller
 {
@@ -29,6 +30,23 @@ class OrderController extends Controller
      */
     public function CreateOrder(Request $request) {
 
+        // check for the input validations
+        $validator = Validator::make($request->all(), [
+            'order' => 'required',
+            'order.customer' => 'required',
+            'order.address' => 'required',
+            'order.items' => 'required'
+        ],[
+            'order.customer.required' => 'Customer name is required',
+            'order.address.required' => 'Customer address is required',
+            'order.items.required' => 'Order items are required'
+        ]);
+
+        // Return errors if validation fails
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()]);
+        }
+
         // Order service to create a new order
 		$order = $this->orderService->createOrder($request->all());
 
@@ -36,7 +54,7 @@ class OrderController extends Controller
         if($order) {
             return response()->json(['success' => 'Order created succesfully']);
         }else{
-            return response()->json(['error' => 'Some error occured. Please try again laer']);
+            return response()->json(['error' => 'Some error occured. Please try again later']);
         }
 
 
